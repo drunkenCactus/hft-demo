8cd298ac84b4ed2c0f72ac54186d1e949f3f4996
Petr <71551255+drunkenCactus@users.noreply.github.com>
2026-02-25 02:27:14 +0300

    add ring buffer (#7)
    Co-authored-by: petr vm <petr@vm-hft-demo.ru-central1.internal>

--- lib/shared_memory.hpp
+++ lib/shared_memory.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <cstdint>
 #include <utility>
 
@@ -13,5 +14,75 @@ struct SharedData {
 
 const char* const SHARED_MEMORY_NAME = "order_book";
 const uint32_t SHARED_MEMORY_SIZE = 64;
+const uint32_t CACHE_LINE_SIZE = 64;
+
+template <typename Data, uint32_t BufferSize, uint32_t ConsumersCount>
+class RingBuffer {
+public:
+    RingBuffer() {
+        head_ = 0;
+        for (uint32_t i = 0; i < ConsumersCount; ++i) {
+            tails_[i] = 0;
+            active_consumers_[i] = true;
+        }
+    }
+
+    RingBuffer(const RingBuffer& other) = delete;
+    RingBuffer(RingBuffer&& other) = delete;
+    RingBuffer& operator=(const RingBuffer& other) = delete;
+    RingBuffer& operator=(RingBuffer&& other) = delete;
+    ~RingBuffer() = default;
+
+    void Write(const Data& data) noexcept {
+        const uint32_t current_head = head_.load(std::memory_order_relaxed);
+        const uint32_t new_head = Increment(current_head);
+        DisableLaggingConsumers(new_head);
+        data_[new_head] = data;
+        head_.store(new_head, std::memory_order_release);
+    }
+
+    bool Read(Data& data, const uint32_t consumer) noexcept {
+        if (!active_consumers_[consumer].load(std::memory_order_acquire)) {
+            // consumer is disabled
+            return false;
+        }
+        const uint32_t current_tail = tails_[consumer].load(std::memory_order_relaxed);
+        const uint32_t head = head_.load(std::memory_order_acquire);
+        if (current_tail == head) {
+            // nothing to read
+            return false;
+        }
+        const uint32_t new_tail = Increment(current_tail);
+        data = data_[new_tail];
+        tails_[consumer].store(new_tail, std::memory_order_release);
+        return true;
+    }
+
+private:
+    uint32_t Increment(const uint32_t current) const noexcept {
+        return (current + 1) % BufferSize;
+    }
+
+    void DisableLaggingConsumers(const uint32_t new_head) noexcept {
+        for (uint32_t i = 0; i < ConsumersCount; ++i) {
+            const uint32_t tail = tails_[i].load(std::memory_order_acquire);
+            if (new_head == tail) {
+                // disable lagging consumer
+                active_consumers_[i].store(false, std::memory_order_release);
+            }
+        }
+    }
+
+private:
+    alignas(CACHE_LINE_SIZE) std::atomic<uint32_t> head_;
+    alignas(CACHE_LINE_SIZE) std::atomic<uint32_t> tails_[ConsumersCount];
+    alignas(CACHE_LINE_SIZE) std::atomic<bool> active_consumers_[ConsumersCount];
+    alignas(CACHE_LINE_SIZE) Data data_[BufferSize];
+
+    static_assert(alignof(Data) == CACHE_LINE_SIZE);
+    static_assert(sizeof(Data) == CACHE_LINE_SIZE);
+    static_assert(std::atomic<uint32_t>::is_always_lock_free);
+    static_assert(std::atomic<bool>::is_always_lock_free);
+};
 
 }  // namespace hft
--- tests/test_buffer.cpp
+++ tests/test_buffer.cpp
@@ -1,9 +1,110 @@
 #include <gtest/gtest.h>
 
-TEST(Buffer, Good) {
-    EXPECT_TRUE(true);
+#include <lib/shared_memory.hpp>
+
+#include <cstdint>
+
+using namespace hft;
+
+constexpr uint32_t BUFFER_SIZE = 8;
+constexpr uint32_t CONSUMERS_COUNT = 2;
+
+struct alignas(CACHE_LINE_SIZE) TestData {
+    char value;
+};
+
+using TestRingBuffer = RingBuffer<TestData, BUFFER_SIZE, CONSUMERS_COUNT>;
+
+void Consume(
+    TestRingBuffer& buffer,
+    const uint32_t consumer_id,
+    std::string& result,
+    const bool& is_running,
+    const uint32_t timeout_ms
+) {
+    while (is_running) {
+        TestData data;
+        const bool has_data = buffer.Read(data, consumer_id);
+        if (has_data) {
+            result += data.value;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(timeout_ms));
+    }
 }
 
-TEST(Buffer, Bad) {
-    EXPECT_TRUE(false);
+void Produce(TestRingBuffer& buffer, const std::string& data) {
+    for (const char item : data) {
+        buffer.Write(TestData{
+            .value = item
+        });
+        std::this_thread::sleep_for(std::chrono::milliseconds(15));
+    }
+}
+
+TEST(RingBuffer, AllFastConsumers) {
+    TestRingBuffer buffer;
+    const std::string data = "c478fy478bcf87ergfw48b7f7ch48hffhewiybfr78ey4378bc487gbci7rghfciucbf3478tr387c";
+    bool is_running = true;
+
+    std::vector<std::string> results(CONSUMERS_COUNT, "");
+
+    std::vector<std::thread> consumers;
+    for (uint32_t consumer_id = 0; consumer_id < CONSUMERS_COUNT; ++consumer_id) {
+        const uint32_t timeout_ms = 10;
+        consumers.emplace_back(
+            Consume,
+            std::ref(buffer),
+            consumer_id,
+            std::ref(results.at(consumer_id)),
+            std::cref(is_running),
+            timeout_ms
+        );
+    }
+
+    Produce(buffer, data);
+    is_running = false;
+    for (auto& consumer : consumers) {
+        consumer.join();
+    }
+
+    for (const auto& result : results) {
+        EXPECT_EQ(data, result);
+    }
+}
+
+TEST(RingBuffer, DisableLaggingConsumer) {
+    TestRingBuffer buffer;
+    const std::string data = "c478fy478bcf87ergfw48b7f7ch48hffhewiybfr78ey4378bc487gbci7rghfciucbf3478tr387c";
+    bool is_running = true;
+
+    std::vector<std::string> results(CONSUMERS_COUNT, "");
+    std::vector<uint32_t> timeouts_ms = {10, 20};
+
+    std::vector<std::thread> consumers;
+    for (uint32_t consumer_id = 0; consumer_id < CONSUMERS_COUNT; ++consumer_id) {
+        consumers.emplace_back(
+            Consume,
+            std::ref(buffer),
+            consumer_id,
+            std::ref(results.at(consumer_id)),
+            std::cref(is_running),
+            timeouts_ms[consumer_id]
+        );
+    }
+
+    Produce(buffer, data);
+    is_running = false;
+    for (auto& consumer : consumers) {
+        consumer.join();
+    }
+
+    {
+        const auto& result = results.at(0);
+        EXPECT_EQ(data, result);
+    }
+    {
+        const auto& result = results.at(1);
+        EXPECT_FALSE(result.empty());
+        EXPECT_TRUE(result.size() < data.size());
+    }
 }
